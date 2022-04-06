1470f7755c1e611503aab0bd820218737dfa78b6
LORDAME0 <unlimit8.com@gmail.com>
2022-04-06 17:36:58 +0000

    trying clion

--- module01/ex04/main.cpp
+++ module01/ex04/main.cpp
@@ -9,60 +9,60 @@ using std::ios;
 using std::ofstream;
 using std::string;
 
-string replace(const string &s, const string target, const string replacment) {
+const string replace(const string &s, const string target, const string replacement) {
 
-  size_t target_index = s.find(target);
-  if (target_index == string::npos)
-    return s;
+    size_t target_index = s.find(target);
+    if (target_index == string::npos)
+        return s;
 
-  string tmp;
-  tmp.append(s.substr(0, target_index));
-  tmp.append(replacment);
-  tmp.append(s.substr(target_index + target.length()));
+    string tmp{};
+    tmp.append(s.substr(0, target_index));
+    tmp.append(replacment);
+    tmp.append(s.substr(target_index + target.length()));
 
-  return replace(tmp, target, replacment);
+    return replace(tmp, target, replacment);
 }
 
 int main(int ac, char **av) {
 
-  if (ac != 4) {
-    cerr << "SFL: <filename> <target> <replacment>\n";
-    return 1;
-  }
+    if (ac != 4) {
+        cerr << "SFL: <filename> <target> <replacement>\n";
+        return 1;
+    }
 
-  const string file_name(av[1]), target(av[2]), replacment(av[3]);
+    const string file_name(av[1]), target(av[2]), replacement(av[3]);
 
-  const string new_file_name(file_name + ".replace");
+    const string new_file_name(file_name + ".replace");
 
-  ifstream file;
-  ofstream new_file;
+    ifstream file;
+    ofstream new_file;
 
-  file.open(file_name.c_str(), ios::in);
-  new_file.open(new_file_name.c_str(), ios::out);
+    file.open(file_name.c_str(), ios::in);
+    new_file.open(new_file_name.c_str(), ios::out);
 
-  if (file.is_open() == false) {
-    string err("SFL : " + file_name + " ");
-    perror(err.c_str());
-    return 1;
-  }
+    if (file.is_open() == false) {
+        string err("SFL : " + file_name + " ");
+        perror(err.c_str());
+        return 1;
+    }
 
-  if (file.peek() == EOF) {
-    cerr << "SFL: empty file\n";
-    return 1;
-  }
+    if (file.peek() == EOF) {
+        cerr << "SFL: empty file\n";
+        return 1;
+    }
 
-  if (new_file.is_open() == false) {
-    string error = "SFL : failed to create '.replace' file ";
-    perror(error.c_str());
-    return 1;
-  }
+    if (new_file.is_open() == false) {
+        string error = "SFL : failed to create '.replace' file ";
+        perror(error.c_str());
+        return 1;
+    }
 
-  string line;
+    string line;
 
-  while (std::getline(file, line)) {
-    new_file << replace(line, target, replacment) << "\n";
-  }
+    while (std::getline(file, line)) {
+        new_file << replace(line, target, replacment) << "\n";
+    }
 
-  file.close();
-  new_file.close();
+    file.close();
+    new_file.close();
 }
